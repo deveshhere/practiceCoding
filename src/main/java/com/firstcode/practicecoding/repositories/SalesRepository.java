4ec89b03cf5fe3e2cc8578f720d25726d77a2aac
Devesh sharma <devesh@technoexponent.com>
2022-02-23 13:50:51 +0530

    Naming conventions sloved

--- src/main/java/com/firstcode/practicecoding/repositories/SalesRepository.java
+++ src/main/java/com/firstcode/practicecoding/repositories/SalesRepository.java
@@ -3,5 +3,5 @@ package com.firstcode.practicecoding.repositories;
 import com.firstcode.practicecoding.entities.Sales;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+@Repository
 public interface SalesRepository extends JpaRepository<Sales, Long> {}
